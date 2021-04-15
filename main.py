f3e2b9d841158da0ec746b3ea957285f1477fb68
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-04-15 12:00:41 +0100

    update env settings

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ APP_FILES = [
 ]
 
 ORIGINS = [
-    "http://127.0.0.1:8080",
+    "http://127.0.0.1:9990",
     "https://api.crunchy.gg",
     "https://crunchy.gg",
 ]
--- utils/settings.py
+++ utils/settings.py
@@ -1,17 +1,19 @@
 import os
 
 
-# Changeable
-secure_sessions = bool(os.getenv("SECURE"))
-
 # Authorization and cookies
 secure_key: str = os.getenv("SECURE_KEY")
-bot_auth: str = os.getenv("BOT_KEY")
+bot_auth: str = os.getenv("BOT_AUTH")
 
 client_id: int = int(os.getenv("CLIENT_ID"))
 client_secret: str = os.getenv("CLIENT_SECRET")
 
 debug: bool = bool(os.getenv("DEBUG", True))
+secure_sessions = not debug
 
 search_engine_domain: str = os.getenv("SEARCH_DOMAIN")
+redirect_url = os.getenv("REDIRECT_URI")
+base_url = os.getenv("BASE_URL")
+
+
 
